36ee513b9969151dd9a1e10ff3ec3c0a137c2606
DavidKritz <70144894+DavidKritz@users.noreply.github.com>
2021-04-14 14:27:23 +0100

    Add files via upload

---
+++ php/addDept.php
@@ -0,0 +1,64 @@
+<?php
+
+	// remove next two lines for production
+
+	$executionStartTime = microtime(true);
+
+	include("config.php");
+
+	header('Content-Type: application/json; charset=UTF-8');
+
+	$conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
+
+	if (mysqli_connect_errno()) {
+		
+		$output['status']['code'] = "300";
+		$output['status']['name'] = "failure";
+		$output['status']['description'] = "database unavailable";
+		$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+		$output['data'] = [];
+
+		mysqli_close($conn);
+
+		echo json_encode($output);
+
+		exit;
+
+	}	
+
+	// $_POST used for development / debugging. Remember to cange to $_POST for production
+
+	$dept = $_POST['dept'];
+	$costCenter = $_POST['costCenter'];
+	$billCode = $_POST['billCode'];	
+	$locationID = $_POST['locationID'];
+
+	$query = "INSERT INTO department (`dept`, `costCenter`, `billCode`, `locationID`) VALUES('$dept','$costCenter','$billCode','$locationID')";
+
+	$result = $conn->query($query);
+	
+	if (!$result) {
+
+		$output['status']['code'] = "400";
+		$output['status']['name'] = "executed";
+		$output['status']['description'] = "query failed";	
+		$output['data'] = [];
+
+		mysqli_close($conn);
+
+		echo json_encode($output); 
+
+		//exit;
+
+	}
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "success";
+	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+	$output['data'] = [];
+	
+	mysqli_close($conn);
+
+	echo json_encode($output); 
+
+?>
---
+++ php/config.php
@@ -0,0 +1,8 @@
+<?php
+
+	$cd_host = 'db5001163672.hosting-data.io';
+	$cd_user = 'dbu993675'; // user name
+	$cd_password = 'EJHUh4xz$G2%'; // password
+	$cd_dbname = 'dbs993843'; // database name
+
+?>
