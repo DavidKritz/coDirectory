dfa252ff8dd1b5baa3c6e5a73f432f51c6c4be37
DavidKritz <70144894+DavidKritz@users.noreply.github.com>
2021-04-20 11:37:57 +0100

    Delete php directory

--- php/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$cd_host = 'db5001163672.hosting-data.io';
-	$cd_user = 'dbu993675'; // user name
-	$cd_password = 'EJHUh4xz$G2%'; // password
-	$cd_dbname = 'dbs993843'; // database name
-
-?>
